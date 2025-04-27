0525db0630dcb6e5b334685ef0e07e0dde0d97e5
Matthias Kluett <mkluett@superfcm.com>
2025-04-27 12:16:41 +0200

    Remove color coding from logger implementation for simplified log output

--- lib/src/utils/logger.dart
+++ lib/src/utils/logger.dart
@@ -14,14 +14,6 @@ enum LogLevel {
 class Logger {
   LogLevel minLevel;
 
-  // ANSI escape codes for colors
-  static const String _gray = '\x1B[90m';
-  static const String _purple = '\x1B[35m';
-  static const String _blue = '\x1B[34m';
-  static const String _yellow = '\x1B[33m';
-  static const String _red = '\x1B[31m';
-  static const String _reset = '\x1B[0m';
-
   Logger({
     this.minLevel = kDefaultLogLevel,
   });
@@ -30,22 +22,6 @@ class Logger {
   /// Messages below this level will not be logged.
   void setLevel(LogLevel level) {
     minLevel = level;
-  }
-
-  /// Get the color code for a specific log level
-  String _getColorForLevel(LogLevel level) {
-    switch (level) {
-      case LogLevel.verbose:
-        return _gray;
-      case LogLevel.debug:
-        return _purple;
-      case LogLevel.info:
-        return _blue;
-      case LogLevel.warning:
-        return _yellow;
-      case LogLevel.error:
-        return _red;
-    }
   }
 
   /// Log a message at level [LogLevel.verbose].
@@ -81,15 +57,11 @@ class Logger {
   /// Log a message with [level].
   void log(LogLevel level, dynamic message) {
     if (level.index >= minLevel.index) {
-      final color = _getColorForLevel(level);
       final now = DateTime.now();
       final isoTimestamp =
           '${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}:${now.second.toString().padLeft(2, '0')}.${now.millisecond.toString().padLeft(3, '0')}';
       final logLevel = level.name.substring(0, 1).toUpperCase();
-      final coloredMessage = color.isNotEmpty
-          ? '$color[$logLevel]: $message$_reset'
-          : '[$logLevel]: $message';
-      debugPrint('[SuperFCM] [$isoTimestamp] $coloredMessage');
+      debugPrint('[SuperFCM] [$isoTimestamp] [$logLevel]: $message');
     }
   }
 }
